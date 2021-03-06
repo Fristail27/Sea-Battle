9191316ae913bc906244a71537dc9006e978542f
hazekost <kostyk0007@gmail.com>
2021-03-06 10:31:51 +0300

    create form for registration page with inputs and button

--- src/pages/registration/Registration.tsx
+++ src/pages/registration/Registration.tsx
@@ -1,9 +1,19 @@
 import React from "react";
-
+import { NavLink } from "react-router-dom";
+import s from "./Registration.module.css"
 
 const Registration = () => {
     return (
-        <h1>Registration</h1>
+        <div className={s.registrationModule}>
+            <h1>Registration</h1>
+            <form className={s.registrationForm}>
+                <input className='email' name="email" placeholder="Email" type={"email"}/>
+                <input data-validate-length="6,8" name="password"  placeholder="Password" type={"password"}/>
+                <input data-validate-length="6,8" name="password"  placeholder="Repeat password" type={"password"}/>
+                <button>Register</button>
+                <NavLink to={"/login"}>Sign in</NavLink>
+            </form>
+        </div>
     )
 }
 
