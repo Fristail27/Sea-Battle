bf4ad0f59cbffc685e71d9de38f77aa7f94cb331
Alexander Sindetckiy <zoomskiy@gmail.com>
2021-03-11 20:57:02 +0300

    Login/Logout button done, navbar done

---
+++ src/common/Navigation/Navigation.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import styles from "./Navigation.module.css"
+import {useDispatch, useSelector} from "react-redux";
+import {AppRootStateType} from "../../store/store";
+import {authTryAC, initialState} from "../../store/auth-Reducer";
+import {onChangeAppStatusAC} from "../../store/app-Reducer";
+
+export const Navigations = () => {
+    const dispatch = useDispatch()
+    const authorized = useSelector<AppRootStateType, boolean>(state => state.auth.isAuth)
+    const authHandler = () => {
+        if(authorized) {
+            dispatch(authTryAC(initialState.userData, false))
+            dispatch(onChangeAppStatusAC("idle"))
+        }
+    }
+    return (
+        <div className={styles.wrapper}>
+            <nav className={styles.navigation}>
+                <Link to={"/"}>Home</Link>
+                <Link to={"/profile"}>Profile</Link>
+            </nav>
+            <button onClick={authHandler}>{authorized ? <span>Logout</span>: <span>Login</span>}</button>
+
+        </div>
+    )
+}
--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import {useSelector} from "react-redux";
+import { Redirect } from "react-router-dom";
+import {AppRootStateType} from "../../store/store";
 
 
 const Profile = () => {
+    const profName = useSelector<AppRootStateType, string | null>(state => state.auth.userData.name)
+    const isAuth = useSelector<AppRootStateType, boolean>(state => state.auth.isAuth)
+    if(!isAuth) {
+        return <Redirect to={'/'}/>
+    }
+
     return (
-        <h1>profile</h1>
+
+        <h1>profile {profName}</h1>
+
     )
 }
 
